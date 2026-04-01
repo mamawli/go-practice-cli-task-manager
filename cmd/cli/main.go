2789d3ca4302dbaa58f8de9b9dbf449c6f1f74c4
Javad <j.nasrolla@rahkar.team>
2026-04-01 17:20:53 +0330

    use interface for storage

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -15,44 +15,56 @@ func main() {
 	doneTaskFlag := flag.Int64("done", 0, "Done task by id")
 	deleteTaskFlag := flag.Int64("delete", 0, "delete task by id")
 
-	used := false
-	flag.VisitAll(func(f *flag.Flag) {
-		used = true
-	})
-
-	if !used {
+	flag.Parse()
+	if flag.NFlag() == 0 {
 		fmt.Println("Invalid Flag")
 		flag.Usage()
 		return
 	}
-
-	storage.AutoCreateDB()
-
-	flag.Parse()
+	jsonStorage := storage.NewJsonStorage()
 
 	if *listFlag {
-		fmt.Println(task.ReadTasks())
+		tasks, err := jsonStorage.List()
+		if err != nil {
+			fmt.Println("Failed to get tasks", err)
+			return
+		}
+		fmt.Println(tasks)
 		return
 	}
 
 	if *addFlag != "" {
 		newTask := task.NewTask(*addFlag)
-		task.AddTask(newTask)
+		err := jsonStorage.Add(newTask)
+		if err != nil {
+			fmt.Println("Failed to add new task", err)
+		}
 		return
 	}
 
 	if *getTaskFlag != 0 {
-		fmt.Println(task.GetTaskById(*getTaskFlag))
+		t, err := jsonStorage.GetByID(*getTaskFlag)
+		if err != nil {
+			fmt.Println("failed to get task by id", *getTaskFlag, err)
+			return
+		}
+		fmt.Println(t)
 		return
 	}
 
 	if *doneTaskFlag != 0 {
-		task.CompleteTask(*doneTaskFlag)
+		err := jsonStorage.MarkDone(*doneTaskFlag)
+		if err != nil {
+			fmt.Println("Failed to mark task to done", err)
+		}
 		return
 	}
 
 	if *deleteTaskFlag != 0 {
-		task.DeleteTask(*deleteTaskFlag)
+		err := jsonStorage.Delete(*deleteTaskFlag)
+		if err != nil {
+			fmt.Println("Failed to delete task", err)
+		}
 		return
 	}
 }
